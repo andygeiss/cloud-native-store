a909416f60e041b246fbb8d33f36f229ebeb4fc8
Andy <4ndygeiss@gmail.com>
2024-12-30 14:25:12 +0100

    feat: add service context

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -6,15 +6,13 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/andygeiss/cloud-native-store/internal/app/adapters/inbound/api"
 	"github.com/andygeiss/cloud-native-store/internal/app/adapters/outbound/gcp"
 	"github.com/andygeiss/cloud-native-store/internal/app/config"
 	"github.com/andygeiss/cloud-native-store/internal/app/core/services"
 	"github.com/andygeiss/cloud-native-utils/security"
+	"github.com/andygeiss/cloud-native-utils/service"
 )
 
 func main() {
@@ -38,57 +36,42 @@ func main() {
 	objectPort := gcp.NewCloudSpanner(cfg)
 
 	// Create a new Object Service.
-	service := services.
+	svc := services.
 		NewObjectService(cfg).
 		WithPort(objectPort)
 
 	// Create a new context with a cancel function.
-	ctx, cancel := signal.NotifyContext(
-		context.Background(),
-		// SIGTERM is sent by Kubernetes to gracefully stop a container.
-		syscall.SIGTERM,
-		// SIGINT is sent by a user terminal to interrupt a running process.
-		syscall.SIGINT,
-		// SIGQUIT is sent by a user terminal to make a core dump.
-		syscall.SIGQUIT,
-		// SIGKILL is sent by a user terminal to kill a process immediately.
-		syscall.SIGKILL,
-	)
+	ctx, cancel := service.Context()
 	defer cancel()
 
 	// Set up the service. If an error occurs during setup, log it and terminate the program.
-	if err := service.Setup(); err != nil {
+	if err := svc.Setup(); err != nil {
 		log.Fatalf("error during setup: %v", err)
 	}
-	// Ensure proper cleanup of resources when the service is no longer needed.
-	defer service.Teardown()
+	defer svc.Teardown()
 
 	// Initialize the API router using the configuration object.
-	mux := api.Route(service, ctx)
+	mux := api.Route(svc, ctx)
 
 	// Create a new secure server.
-	server := security.NewServer(mux)
-	defer server.Close()
+	srv := security.NewServer(mux)
+	defer srv.Close()
 
 	// Register the server shutdown function.
-	// This function is called when the server is shut down.
-	server.RegisterOnShutdown(func() {
+	srv.RegisterOnShutdown(func() {
 		// TODO: Add cleanup logic here.
 	})
 
-	// Run the context check in a separate goroutine.
-	go func() {
-		<-ctx.Done()
-		// Wait for the readiness check to fail.
-		<-time.After(5 * time.Second)
-		// Shut down the server using a new context.
-		// Do not use the original (already closed) context.
-		server.Shutdown(context.Background())
-	}()
+	// Register the service shutdown function.
+	service.RegisterOnContextDone(ctx, func() {
+		// Avoid using the already closed context to prevent a panic.
+		// Thus we use a new context to shut down the server.
+		srv.Shutdown(context.Background())
+	})
 
 	// Start the HTTP server in the main goroutine.
 	log.Printf("start listening at port %s ...", cfg.Server.Port)
-	if err := server.ListenAndServe(); err != nil {
+	if err := srv.ListenAndServe(); err != nil {
 		// Check if the server was closed intentionally.
 		if err == http.ErrServerClosed {
 			log.Println("server is closed.")
